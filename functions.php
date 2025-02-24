ce12dc202df0f260706c16799717e5bd92a722a9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-02-24 14:47:54 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,28 +18,76 @@ require_once('includes/shortcodes.php');
 require_once('includes/post-types.php');
 
 
-function change_mt_listing_slug( $args, $post_type ) {
-    if ( 'mt_listing' === $post_type ) {
+function change_mt_listing_slug($args, $post_type)
+{
+    if ('mt_listing' === $post_type) {
         $args['rewrite']['slug'] = 'resources';
     }
     return $args;
 }
-add_filter( 'register_post_type_args', 'change_mt_listing_slug', 10, 2 );
+add_filter('register_post_type_args', 'change_mt_listing_slug', 10, 2);
 
 
-function change_mt_listing_category2_slug( $args, $taxonomy ) {
-    if ( 'mt-listing-category2' === $taxonomy ) {
+function change_mt_listing_category2_slug($args, $taxonomy)
+{
+    if ('mt-listing-category2' === $taxonomy) {
         $args['rewrite']['slug'] = 'resources-category';
     }
     return $args;
 }
-add_filter( 'register_taxonomy_args', 'change_mt_listing_category2_slug', 10, 2 );
+add_filter('register_taxonomy_args', 'change_mt_listing_category2_slug', 10, 2);
 
 
-function memberplace_marketplace() {
+function memberplace_marketplace()
+{
     ob_start();
-    ?>
+?>
     
     <?php
     return ob_get_clean();
+}
+
+function _author_email($post_author)
+{
+    $user = get_userdata($post_author);
+    if ($user && isset($user->user_email)) {
+        return $user->user_email;
+    } else {
+        return false; // Or handle the error as needed
+    }
+}
+
+
+function _author_company($post_author)
+{
+    $organisation = get_user_meta($post_author, 'organisation', true);
+
+    if ($organisation) {
+        return $organisation;
+    } else {
+        return false; // Or handle the absence of the meta value as needed
+    }
+}
+
+
+function _author_name($post_author)
+{
+    $user = get_userdata($post_author);
+
+    if ($user) {
+        $first_name = $user->first_name;
+        $last_name  = $user->last_name;
+
+        if (! empty($first_name) && ! empty($last_name)) {
+            return $first_name . ' ' . $last_name;
+        } elseif (! empty($first_name)) {
+            return $first_name;
+        } elseif (!empty($last_name)) {
+            return $last_name;
+        } else {
+            return $user->display_name; // Fallback to display name if first/last names are empty
+        }
+    } else {
+        return false; // Or handle the error as needed
+    }
 }
