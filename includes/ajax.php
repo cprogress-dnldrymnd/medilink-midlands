e22d521d1cce5d193a34e514e92cc86b52df29a0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-06 20:54:19 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -14,7 +14,7 @@ function ajax_post_loader_load_more()
     $args = array(
         'post_status' => 'publish',
         'post_type' => 'wpsl_stores',
-        'posts_per_page' => 10,
+        'posts_per_page' => -1,
         'paged' => $paged,
         'orderby' => 'title',
         'order' => 'ASC'
@@ -32,6 +32,8 @@ function ajax_post_loader_load_more()
         }
         while ($query->have_posts()) {
             $query->the_post();
+            $first_letter = $getFirstLetter(get_the_title());
+            echo $first_letter;
             echo membership_listing();
         }
         wp_reset_postdata();
@@ -43,3 +45,11 @@ function ajax_post_loader_load_more()
 }
 add_action('wp_ajax_nopriv_ajax_post_loader_load_more', 'ajax_post_loader_load_more');
 add_action('wp_ajax_ajax_post_loader_load_more', 'ajax_post_loader_load_more');
+
+function getFirstLetter($string) {
+    if (empty($string)) {
+      return ""; // Return an empty string if the input is empty
+    }
+  
+    return $string[0]; // Access the first character using array-like notation
+  }
