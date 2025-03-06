7fe9aa0f88237e0f2d290f605c4eaa0206f9f914
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-06 19:12:35 +0800

    cs

--- footer.php
+++ footer.php
@@ -81,38 +81,7 @@ if (is_user_logged_in()) {
 }
 ?>
 
-<script>
-    jQuery(document).ready(function() {
-        jQuery('<li id="join-us-button" ><a href="/join-us/"><?= $button ?></a> </li>').appendTo('header .nav-menu');
-        jQuery('.select-2-trigger select').select2({
-            maximumSelectionLength: 3,
-        });
 
-        if (jQuery('.package-price-main-details-inner').length > 0) {
-            matchHeights('.package-price-main-details-inner');
-        }
-
-        jQuery('.learn-more a').click(function(e) {
-            jQuery(this).parents('.inner').addClass('package-active');
-            e.preventDefault();
-        });
-    });
-
-    function matchHeights(selector) {
-        var maxHeight = 0;
-
-        // Reset heights before calculating
-        jQuery(selector).css('height', 'auto');
-
-        // Find the tallest element
-        jQuery(selector).each(function() {
-            maxHeight = Math.max(maxHeight, jQuery(this).outerHeight());
-        });
-
-        // Set all elements to the tallest height
-        jQuery(selector).height(maxHeight);
-    }
-</script>
 
 <?php wp_footer(); ?>
 </body>
