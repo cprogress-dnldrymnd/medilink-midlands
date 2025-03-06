cf548e0535d77669c038adad14c0eff0654ffd92
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-06 20:27:41 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -4,8 +4,6 @@ function ajax_post_loader_load_more()
     check_ajax_referer('ajax_post_loader_nonce', 'security');
 
     $paged = isset($_POST['paged']) ? intval($_POST['paged']) : 1;
-    $directory_filter = isset($_POST['directory_filter']) ? $_POST['directory_filter'] : false;
-    $search_var = isset($_POST['search_var']) ? $_POST['search_var'] : false;
 
     $args = array(
         'post_status' => 'publish',
@@ -16,9 +14,7 @@ function ajax_post_loader_load_more()
         'order' => 'ASC'
     );
 
-    if ($search_var) {
-        $args['s'] = $search_var;
-    }
+ 
     $query = new WP_Query($args);
 
     if ($query->have_posts()) {
