f7c1025966b62e0da744684d005dc48a632b6dd8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-14 17:37:39 +0800

    Update template-header2.php

--- templates/template-header2.php
+++ templates/template-header2.php
@@ -3,137 +3,137 @@
   <!-- BOTTOM BAR -->
   <nav class="navbar navbar-default logo-infos" id="modeltheme-main-head">
     <div class="container">
-        <div class="row">
+      <div class="row">
 
-          <!-- LOGO -->
-          <div class="navbar-header col-md-3">
-            <!-- NAVIGATION BURGER MENU -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+        <!-- LOGO -->
+        <div class="navbar-header col-md-3">
+          <!-- NAVIGATION BURGER MENU -->
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
 
-            <?php if ( class_exists( 'ReduxFrameworkPlugin' ) ) { 
-              $custom_header_activated = get_post_meta( get_the_ID(), 'smartowl_custom_header_options_status', true );
-              $header_v = get_post_meta( get_the_ID(), 'smartowl_header_custom_variant', true );
-              $custom_logo_url = get_post_meta( get_the_ID(), 'smartowl_header_custom_logo', true );
+          <?php if (class_exists('ReduxFrameworkPlugin')) {
+            $custom_header_activated = get_post_meta(get_the_ID(), 'smartowl_custom_header_options_status', true);
+            $header_v = get_post_meta(get_the_ID(), 'smartowl_header_custom_variant', true);
+            $custom_logo_url = get_post_meta(get_the_ID(), 'smartowl_header_custom_logo', true);
 
-              if($custom_header_activated == 'yes' && isset($custom_logo_url) && !empty($custom_logo_url)) { ?>
+            if ($custom_header_activated == 'yes' && isset($custom_logo_url) && !empty($custom_logo_url)) { ?>
 
-                <h1 class="logo">
-                    <a href="<?php echo esc_url(get_site_url()); ?>">
-                        <img src="<?php echo esc_url($custom_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo()); ?>" />
-                    </a>
-                </h1>
+              <h1 class="logo">
+                <a href="<?php echo esc_url(get_site_url()); ?>">
+                  <img src="<?php echo esc_url($custom_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo()); ?>" />
+                </a>
+              </h1>
 
               <?php } else {
 
-                if(wikb('mt_logo','url')){ ?>
-                  <div class="logo">
-                      <a href="<?php echo esc_url(get_site_url()); ?>">
-                          <img src="<?php echo esc_url(wikb('mt_logo','url')); ?>" alt="<?php echo esc_attr(get_bloginfo()); ?>" />
-                      </a>
-                  </div>
-                <?php }else{ ?>
-                  <div class="logo no-logo">
-                      <a href="<?php echo esc_url(get_site_url()); ?>">
-                        <?php echo esc_html(get_bloginfo()); ?>
-                      </a>
-                  </div>
-                <?php } ?>
-              <?php } ?>
-
-            <?php }else{ ?>
-              <div class="logo no-logo">
+              if (wikb('mt_logo', 'url')) { ?>
+                <div class="logo">
+                  <a href="<?php echo esc_url(get_site_url()); ?>">
+                    <img src="<?php echo esc_url(wikb('mt_logo', 'url')); ?>" alt="<?php echo esc_attr(get_bloginfo()); ?>" />
+                  </a>
+                </div>
+              <?php } else { ?>
+                <div class="logo no-logo">
                   <a href="<?php echo esc_url(get_site_url()); ?>">
                     <?php echo esc_html(get_bloginfo()); ?>
                   </a>
-              </div>
+                </div>
+              <?php } ?>
             <?php } ?>
-          </div>
 
-          <!-- NAV MENU -->
-          <div id="navbar" class="navbar-collapse collapse col-md-9">
-            <ul class="menu nav navbar-nav pull-right nav-effect nav-menu">
-              <?php
-                if ( has_nav_menu( 'primary' ) ) {
-                  $defaults = array(
-                    'menu'            => '',
-                    'container'       => false,
-                    'container_class' => '',
-                    'container_id'    => '',
-                    'menu_class'      => 'menu',
-                    'menu_id'         => '',
-                    'echo'            => true,
-                    'fallback_cb'     => false,
-                    'before'          => '',
-                    'after'           => '',
-                    'link_before'     => '',
-                    'link_after'      => '',
-                    'items_wrap'      => '%3$s',
-                    'depth'           => 0,
-                    'walker'          => ''
-                  );
+          <?php } else { ?>
+            <div class="logo no-logo">
+              <a href="<?php echo esc_url(get_site_url()); ?>">
+                <?php echo esc_html(get_bloginfo()); ?>
+              </a>
+            </div>
+          <?php } ?>
+        </div>
 
-                  $defaults['theme_location'] = 'primary';
+        <!-- NAV MENU -->
+        <div id="navbar" class="navbar-collapse collapse col-md-9">
+          <ul class="menu nav navbar-nav pull-right nav-effect nav-menu">
+            <?php
+            if (has_nav_menu('primary')) {
+              $defaults = array(
+                'menu'            => '',
+                'container'       => false,
+                'container_class' => '',
+                'container_id'    => '',
+                'menu_class'      => 'menu',
+                'menu_id'         => '',
+                'echo'            => true,
+                'fallback_cb'     => false,
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '%3$s',
+                'depth'           => 0,
+                'walker'          => ''
+              );
 
-                  wp_nav_menu( $defaults );
-                  /* my account */
-                  if (function_exists('run_simple_support_system') && class_exists( 'ReduxFrameworkPlugin' )) { ?>
+              $defaults['theme_location'] = 'primary';
 
-                    <?php $switch = wikb('mt_header_is_account');
-                    if($switch == 1) { ?>
-       
-                        <?php if (is_user_logged_in()) { ?> <!-- logged in -->
+              wp_nav_menu($defaults);
+              /* my account */
+              if (function_exists('run_simple_support_system') && class_exists('ReduxFrameworkPlugin')) { ?>
 
-                          <li class="menu-item mt-header-account">
+                <?php $switch = wikb('mt_header_is_account');
+                if ($switch == 1) { ?>
 
-                            <div id="dropdown-user-profile" class="ddmenu">
-                              <a class="profile">
-                                <i class="fa fa-user" aria-hidden="true"></i>
-                              </a>
-                              <ul>
-                                <?php $mt_purchases_link_page = wikb('mt_purchases_link_page'); ?>
-                                <?php $mt_tickets_link_page = wikb('mt_tickets_link_page'); ?>
-                                <?php if(!empty($mt_purchases_link_page)) { ?>
-                                  <li><a href="<?php echo esc_url($mt_purchases_link_page); ?>">
-                                    <i class="icon-bag icons"></i> <?php echo esc_html__('My Purchases','wikb'); ?></a>
-                                  </li>
-                                <?php } ?>
-                                <?php if(!empty($mt_tickets_link_page)) { ?>
-                                  <li><a href="<?php echo esc_url($mt_tickets_link_page); ?>">
-                                    <i class="icon-layers icons"></i> <?php echo esc_html__('Create ticket','wikb'); ?></a>
-                                  </li>
-                                <?php } ?>
-                                <li><a href="<?php echo do_shortcode('[um_author_profile_link raw=1]') ?>">
-                                  <i class="icon-user icons"></i> <?php echo esc_html__('Profile','wikb'); ?></a>
-                                </li>
-                                <div class="dropdown-divider"></div>
-                                <li><a href="<?php echo esc_url(wp_logout_url( home_url() )); ?>">
-                                  <i class="icon-logout icons"></i> <?php echo esc_html__('Log Out','wikb'); ?></a>
-                                </li>
-                              </ul>
-                            </div>
-                          </li>                    
-                    
-                        <?php } else { ?> <!-- logged out -->
-                          <li id="nav-menu-login" class="nav-menu-account meraki-logoin"><a href="<?php echo esc_url('#'); ?>" data-modal="modal-log-in" class="modeltheme-trigger"><?php esc_html_e('Sign In','wikb'); ?></a>
-                            <?php $mt_login_link_page = wikb('mt_login_link_page'); ?>
+                  <?php if (is_user_logged_in()) { ?> <!-- logged in -->
+
+                    <li class="menu-item mt-header-account">
+
+                      <div id="dropdown-user-profile" class="ddmenu">
+                        <a class="profile">
+                          <i class="fa fa-user" aria-hidden="true"></i>
+                        </a>
+                        <ul>
+                          <?php $mt_purchases_link_page = wikb('mt_purchases_link_page'); ?>
+                          <?php $mt_tickets_link_page = wikb('mt_tickets_link_page'); ?>
+                          <?php if (!empty($mt_purchases_link_page)) { ?>
+                            <li><a href="<?php echo esc_url($mt_purchases_link_page); ?>">
+                                <i class="icon-bag icons"></i> <?php echo esc_html__('My Purchases', 'wikb'); ?></a>
+                            </li>
+                          <?php } ?>
+                          <?php if (!empty($mt_tickets_link_page)) { ?>
+                            <li><a href="<?php echo esc_url($mt_tickets_link_page); ?>">
+                                <i class="icon-layers icons"></i> <?php echo esc_html__('Create ticket', 'wikb'); ?></a>
+                            </li>
+                          <?php } ?>
+                          <li><a href="<?php echo do_shortcode('[um_author_profile_link raw=1]') ?>">
+                              <i class="icon-user icons"></i> <?php echo esc_html__('Profile', 'wikb'); ?></a>
                           </li>
-                        <?php } ?>
-                    <?php } ?> <!--  switch  -->
-                  <?php } /* function exist */
-                }else{
-                  echo '<p class="no-menu text-left">';
-                    echo esc_html__('Primary navigation menu is missing. ', 'wikb');
-                  echo '</p>';
-                }
-              ?>
-            </ul>
-          </div>
+                          <div class="dropdown-divider"></div>
+                          <li><a href="<?php echo esc_url(wp_logout_url(home_url())); ?>">
+                              <i class="icon-logout icons"></i> <?php echo esc_html__('Log Out', 'wikb'); ?></a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+
+                  <?php } else { ?> <!-- logged out -->
+                    <li id="nav-menu-login" class="nav-menu-account meraki-logoin"><a href="<?php echo esc_url('#'); ?>" data-modal="modal-log-in" class="modeltheme-trigger"><?php esc_html_e('Sign In', 'wikb'); ?></a>
+                      <?php $mt_login_link_page = wikb('mt_login_link_page'); ?>
+                    </li>
+                  <?php } ?>
+                <?php } ?> <!--  switch  -->
+            <?php } /* function exist */
+            } else {
+              echo '<p class="no-menu text-left">';
+              echo esc_html__('Primary navigation menu is missing. ', 'wikb');
+              echo '</p>';
+            }
+            ?>
+          </ul>
         </div>
       </div>
+    </div>
   </nav>
 </header>
