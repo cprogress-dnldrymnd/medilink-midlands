2e49227cbf56d3dcd5146425b3174edcb99a66b2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-31 20:07:49 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1249,6 +1249,7 @@ function join_us_v2()
                                         echo "<li>$text</li>";
                                     }
                                 }
+                                $text = false;
                                 ?>
                             <?php } ?>
                         </ul>
