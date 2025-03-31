aa50859581d5a73ce947175aa19287ecf65f0a12
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-31 20:02:01 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1241,7 +1241,7 @@ function join_us_v2()
                                     $text = false;
                                 }
 
-                                if (str_replace(' ', '', $text)) {
+                                if ($text && $text != ' ') {
                                     echo "<li>$text</li>";
                                 } else {
                                     if (has_term($benefits->slug, $taxonomy, $package->ID)) {
