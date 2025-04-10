ce12d401c07a4c2a368e3d955066af69e0c89020
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-10 17:31:07 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -24,30 +24,6 @@ Container::make('post_meta', 'Resources')
 /* Membership Marketplace
 /*-----------------------------------------------------------------------------------*/
 
-Container::make('theme_options', 'Packages')
-	->add_fields(
-		array(
-			Field::make('complex', 'packages', __(''))
-				->add_fields(array(
-					Field::make('text', 'package_name', __('package_name')),
-					Field::make('rich_text', 'package_short_description', __('short_description')),
-					Field::make('text', 'package_core_benefits_title', __('core_benefits_title')),
-					Field::make('rich_text', 'package_core_benefits', __('core_benefits')),
-					Field::make('rich_text', 'package_additional_benefits', __('additional_benefits')),
-					Field::make('rich_text', 'package_discounts', __('discounts')),
-					Field::make('text', 'package_member_level', __('package_member_level')),
-					Field::make('text', 'package_price', __('package_price')),
-				))
-				->set_header_template('<%- package_name %>')
-				->set_layout('tabbed-vertical')
-		)
-	);
-
-
-/*-----------------------------------------------------------------------------------*/
-/* Membership Marketplace
-/*-----------------------------------------------------------------------------------*/
-
 Container::make('post_meta', 'Submitted By')
 	->where('post_type', '=', 'post')
 	->set_priority('high')
