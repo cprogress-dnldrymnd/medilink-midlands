a9936a0a43254142b9018ef58da6b4f9740faff4
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-04-17 21:00:19 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,8 +17,8 @@ function wikb_child_scripts()
     wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/js/main.js');
     wp_localize_script('main', 'ajax_post_loader_params', array(
         'ajax_url' => admin_url('admin-ajax.php'),
-        'paged' => 2,
-        'nonce' => wp_create_nonce('ajax_post_loader_nonce'),
+        'paged'    => 2,
+        'nonce'    => wp_create_nonce('ajax_post_loader_nonce'),
     ));
 }
 add_action('wp_enqueue_scripts', 'wikb_child_scripts');
@@ -58,7 +58,7 @@ add_filter('register_taxonomy_args', 'change_mt_listing_category2_slug', 10, 2);
 function memberplace_marketplace()
 {
     ob_start();
-?>
+    ?>
     <div class="post-box-holder flex-row">
         <div class="row">
             <?php while (have_posts()) {
@@ -89,7 +89,12 @@ function memberplace_marketplace()
                         </div>
                         <div class="bottom">
                             <div class="modeltheme_button">
-                                <a class="button-winona button-green btn btn-sm wow-modal-id-1 claim-offer-button" offer_owner_company="<?= _author_company($post_author) ?>" offer_owner_email="<?= _author_email($post_author) ?>" offer_details="<?= wpautop(get_the_content()) ?>" offer_image="<?= $offer_image ?>" offer_owner="<?= _author_name($post_author) ?>" offer_title="<?= get_the_title() ?>">Claim Offer</a>
+                                <a class="button-winona button-green btn btn-sm wow-modal-id-1 claim-offer-button"
+                                    offer_owner_company="<?= _author_company($post_author) ?>"
+                                    offer_owner_email="<?= _author_email($post_author) ?>"
+                                    offer_details="<?= wpautop(get_the_content()) ?>" offer_image="<?= $offer_image ?>"
+                                    offer_owner="<?= _author_name($post_author) ?>" offer_title="<?= get_the_title() ?>">Claim
+                                    Offer</a>
                             </div>
                         </div>
                     </div>
@@ -97,7 +102,7 @@ function memberplace_marketplace()
             <?php } ?>
         </div>
     </div>
-<?php
+    <?php
     return ob_get_clean();
 }
 
@@ -106,7 +111,8 @@ function _author_email($post_author)
     $user = get_userdata($post_author);
     if ($user && isset($user->user_email)) {
         return $user->user_email;
-    } else {
+    }
+    else {
         return false; // Or handle the error as needed
     }
 }
@@ -118,7 +124,8 @@ function _author_company($post_author)
 
     if ($organisation) {
         return $organisation;
-    } else {
+    }
+    else {
         return false; // Or handle the absence of the meta value as needed
     }
 }
@@ -130,18 +137,22 @@ function _author_name($post_author)
 
     if ($user) {
         $first_name = $user->first_name;
-        $last_name  = $user->last_name;
+        $last_name = $user->last_name;
 
-        if (! empty($first_name) && ! empty($last_name)) {
+        if (!empty($first_name) && !empty($last_name)) {
             return $first_name . ' ' . $last_name;
-        } elseif (! empty($first_name)) {
+        }
+        elseif (!empty($first_name)) {
             return $first_name;
-        } elseif (!empty($last_name)) {
+        }
+        elseif (!empty($last_name)) {
             return $last_name;
-        } else {
+        }
+        else {
             return $user->display_name; // Fallback to display name if first/last names are empty
         }
-    } else {
+    }
+    else {
         return false; // Or handle the error as needed
     }
 }
@@ -151,7 +162,8 @@ function _author_logo($post_author)
     $member_folder = "/wp-content/uploads/ultimatemember/$post_author/";
     if ($organisation) {
         return $member_folder . $organisation;
-    } else {
+    }
+    else {
         return false; // Or handle the absence of the meta value as needed
     }
 }
@@ -178,7 +190,7 @@ function save_cf7_to_custom_post($contact_form)
         $post_title = isset($posted_data['submit_offer_title']) ? sanitize_text_field($posted_data['submit_offer_title']) : 'Contact Form Submission';
         $post_content = isset($posted_data['submit_offer_details']) ? wp_kses_post($posted_data['submit_offer_details']) : '';
         $submit_offer_supporting_resource = isset($posted_data['submit_offer_supporting_resource']) ? $posted_data['submit_offer_supporting_resource'] : false;
-        $submit_offer_supporting_image = isset($posted_data['submit_offer_supporting_image']) ? $posted_data['submit_offer_supporting_image'][0]  : false;
+        $submit_offer_supporting_image = isset($posted_data['submit_offer_supporting_image']) ? $posted_data['submit_offer_supporting_image'][0] : false;
         $submit_offer_category = isset($posted_data['submit_offer_category']) ? $posted_data['submit_offer_category'] : false;
         $submit_offer_user_id = isset($posted_data['submit_offer_user_id']) ? $posted_data['submit_offer_user_id'] : false;
 
@@ -210,11 +222,12 @@ function save_cf7_to_custom_post($contact_form)
                 wp_set_post_terms($post_id, $submit_offer_category_arr, 'membersmarketplace_category');
             }
         }
-    } else if ($form_id == 50282) {
+    }
+    else if ($form_id == 50282) {
         // Sanitize and validate data (crucial!).
         $post_title = isset($posted_data['submit_blog_title']) ? sanitize_text_field($posted_data['submit_blog_title']) : 'Contact Form Submission';
         $post_content = isset($posted_data['submit_blog_content']) ? wp_kses_post($posted_data['submit_blog_content']) : '';
-        $submit_blog_featured_image = isset($posted_data['submit_blog_featured_image']) ? $posted_data['submit_blog_featured_image'][0]  : false;
+        $submit_blog_featured_image = isset($posted_data['submit_blog_featured_image']) ? $posted_data['submit_blog_featured_image'][0] : false;
         $submit_blog_user_id = isset($posted_data['submit_blog_user_id']) ? $posted_data['submit_blog_user_id'] : false;
         $submit_blog_category = isset($posted_data['submit_blog_category']) ? $posted_data['submit_blog_category'][0] : false;
 
@@ -229,7 +242,8 @@ function save_cf7_to_custom_post($contact_form)
 
         if (is_user_logged_in()) {
             $post_data['post_author'] = $submit_blog_user_id;
-        } else {
+        }
+        else {
             $submit_blog_full_name = isset($posted_data['submit_blog_full_name']) ? wp_kses_post($posted_data['submit_blog_full_name']) : '';
             $submit_blog_email_address = isset($posted_data['submit_blog_email_address']) ? wp_kses_post($posted_data['submit_blog_email_address']) : '';
             $submit_blog_organisation = isset($posted_data['submit_blog_organisation']) ? wp_kses_post($posted_data['submit_blog_organisation']) : '';
@@ -326,7 +340,7 @@ function upload_file($file_url, $post_id = 0)
 function wikb_header_title_breadcrumbs_v2($heading, $desc)
 {
     ob_start();
-?>
+    ?>
     <div class="header-title-breadcrumb header-title-breadcrumb-custom relative">
         <div class="header-title-breadcrumb-overlay text-center">
             <div class="container">
@@ -350,7 +364,9 @@ function wikb_header_title_breadcrumbs_v2($heading, $desc)
     return ob_get_clean();
 }
 
-function action_wp_body_class() {}
+function action_wp_body_class()
+{
+}
 
 add_filter('body_class', 'custom_class');
 function custom_class($classes)
@@ -370,7 +386,7 @@ function action_wp_footer()
     $title_area_button_link = get_post_meta(get_the_ID(), 'title_area_button_link', true);
     if ($title_area_description || $title_area_button_text) { ?>
         <script>
-            jQuery(document).ready(function() {
+            jQuery(document).ready(function () {
                 <?php if ($title_area_description) { ?>
                     jQuery('<div class="title-area-desc"><?= $title_area_description ?></div>').insertAfter('.breadcrumb');
                 <?php } ?>
@@ -379,7 +395,7 @@ function action_wp_footer()
                 <?php } ?>
             });
         </script>
-    <?php
+        <?php
     }
 }
 
@@ -402,7 +418,7 @@ function action_admin_head()
             display: none;
         }
     </style>
-<?php
+    <?php
 }
 add_action('admin_head', 'action_admin_head');
 
@@ -440,7 +456,7 @@ function um_notify_admin_on_account_update($user_id, $changes)
     // Get admin email address
     $ultimate_member_options = get_option('um_options');
     if (isset($ultimate_member_options['admin_email'])) {
-        $admin_email =  $ultimate_member_options['admin_email'];
+        $admin_email = $ultimate_member_options['admin_email'];
     }
 
     $user_meta_previous = get_user_meta($user_id, 'user_meta_previous', true);
@@ -448,14 +464,14 @@ function um_notify_admin_on_account_update($user_id, $changes)
 
     if ($admin_email) {
         $user_info = get_userdata($user_id);
-        $username  = $user_info->user_login;
+        $username = $user_info->user_login;
         $user_email = $user_info->user_email;
 
         $subject = sprintf('[%s] User Account Updated', get_bloginfo('name'));
         $message = sprintf('A user has updated their account details on %s.', get_bloginfo('name')) . "\r\n\r\n";
         $message .= sprintf('Username: %s (%s)', $username, $user_email) . "\r\n\r\n";
         $message .= "Changes:\r\n";
-        foreach ($user_meta_previous as $key_previous =>  $previous) {
+        foreach ($user_meta_previous as $key_previous => $previous) {
             $prev_val = $previous;
             $new_val = $changes[$key_previous];
             if ($prev_val != $new_val) {
@@ -483,7 +499,7 @@ add_action('um_user_before_updating_profile', 'my_user_before_updating_profile',
 function my_user_before_updating_profile($userinfo)
 {
     $user_meta_previous['job_role'] = get_user_meta($userinfo['ID'], 'job_role', true);
-    $user_meta_previous['organisation']  = get_user_meta($userinfo['ID'], 'organisation', true);
+    $user_meta_previous['organisation'] = get_user_meta($userinfo['ID'], 'organisation', true);
     $user_meta_previous['phone_number'] = get_user_meta($userinfo['ID'], 'phone_number', true);
     $user_meta_previous['address'] = get_user_meta($userinfo['ID'], 'address', true);
     $user_meta_previous['ttle'] = get_user_meta($userinfo['ID'], 'ttle', true);
@@ -498,69 +514,63 @@ function email_template($display_name, $changes)
     $site_url = get_site_url();
 
     ob_start();
-?>
-    <div style='max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666'>
+    ?>
+    <div
+        style='max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666'>
         <div style='color: #444444;font-weight: normal'>
-            <div style='text-align: center'><img src='<?= $site_url ?>/wp-content/uploads/2025/01/medlink-logo-1.png' alt='' /></div>
-            <div style='text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee'><?= $site_name ?></div>
+            <div style='text-align: center'><img src='<?= $site_url ?>/wp-content/uploads/2025/01/medlink-logo-1.png'
+                    alt='' /></div>
+            <div
+                style='text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee'>
+                <?= $site_name ?></div>
             <div style='clear: both'> </div>
         </div>
         <div style='padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee'>
-            <div style='padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px'><?= $display_name ?> has just updated their information.</div>
+            <div style='padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px'><?= $display_name ?> has just
+                updated their information.</div>
 
             <div style='padding: 0 0 15px 0'>
-                <div style='background: #eee;color: #444;padding: 12px 15px;border-radius: 3px;font-weight: bold;font-size: 16px'>Here are the changes on the account:<br /><br />
+                <div
+                    style='background: #eee;color: #444;padding: 12px 15px;border-radius: 3px;font-weight: bold;font-size: 16px'>
+                    Here are the changes on the account:<br /><br />
                     <?= $changes ?>
                 </div>
             </div>
         </div>
         <div style='color: #999;padding: 20px 30px'>
             <div>Thank you!</div>
-            <div>The <a style='color: #3ba1da;text-decoration: none' href='<?= $site_url ?>'><?= $site_name ?></a> Team</div>
+            <div>The <a style='color: #3ba1da;text-decoration: none' href='<?= $site_url ?>'><?= $site_name ?></a> Team
+            </div>
         </div>
     </div>
-<?php
+    <?php
     return ob_get_clean();
 }
 
-function wpse27856_set_content_type(){
+function wpse27856_set_content_type()
+{
     return "text/html";
 }
-add_filter( 'wp_mail_content_type','wpse27856_set_content_type' );
+add_filter('wp_mail_content_type', 'wpse27856_set_content_type');
 
-/**
- * Add a new custom profile tab to WP Ultimate Member.
- *
- * @param array $tabs Array of existing profile tabs.
- * @return array Updated array of profile tabs.
- */
-function my_um_add_custom_profile_tab($tabs)
+function um_change_posts_text($translation, $text, $domain)
 {
-    $tabs['my_custom_tab'] = array(
-        'name'             => __('My Custom Tab', 'your-text-domain'), // The name of your tab
-        'icon'             => 'um-faicon-star', // Font Awesome icon class (optional)
-        'priority'         => 100, // Adjust the order of the tab
-        'content_callback' => 'my_um_custom_tab_content', // Function to display the tab content
-    );
-    return $tabs;
-}
-add_filter('um_profile_tabs', 'my_um_add_custom_profile_tab', 100);
-
-/**
- * Callback function to display the content of the custom profile tab.
- *
- * @param array $args Array of arguments (usually empty in this context).
- */
-function my_um_custom_tab_content($args)
-{
-    // Output the content you want to display in your custom tab here.
-    echo '<div class="um-profile-body">';
-    echo '<p>This is the content of my custom profile tab!</p>';
-    // You can fetch and display user-specific data here using UM functions.
-    $user_id = um_profile_id(); // Get the ID of the currently viewed user.
-    $custom_field_value = get_user_meta($user_id, 'your_custom_field_key', true);
-    if ($custom_field_value) {
-        echo '<p>Your Custom Field Value: ' . esc_html($custom_field_value) . '</p>';
+    if ('ultimate-member' === $domain) {
+        if ('Posts' === $text) {
+            return 'Articles';
+        }
+        elseif ('View Posts' === $text) {
+            return 'View Articles';
+        }
+        elseif ('No Posts Yet.' === $text) {
+            return 'No Articles Yet.';
+        }
+        elseif ('load more posts' === $text) {
+            return 'load more articles';
+        }
+        // Add more conditions as needed for other instances of "Posts"
     }
-    echo '</div>';
+    return $translation;
 }
+add_filter('gettext', 'um_change_posts_text', 20, 3);
+add_filter('ngettext', 'um_change_posts_text', 20, 3);
