5c4ebd6950b5639a5b39d0eda2f9c3c9c5daad5c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-25 00:51:27 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -660,7 +660,7 @@ function _claim_offer_button($id)
             $doc_url = wp_get_attachment_url($document["resource"]);
             $file_path = get_attached_file($document["resource"]);
             $file_name = basename($file_path);
-            $documents_html .= "<div class='offer-document'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-file-earmark-text-fill' viewBox='0 0 16 16'> <path d='M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z'/> </svg> <a href='$doc_url'> $file_name </a> </div>";
+            $documents_html .= "<div class='offer-document'> <a href='$doc_url'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-file-earmark-text-fill' viewBox='0 0 16 16'> <path d='M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z'/> </svg> $file_name </a> </div>";
         }
         $documents_html .= "</div>";
         $documents_html .= "</div>";
