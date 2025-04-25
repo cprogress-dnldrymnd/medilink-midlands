fc63971e10094496f475da19f7df3a8e77b5d1f9
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-04-25 23:33:30 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1384,7 +1384,7 @@ function user_marketplace()
     if ($membersmarketplace && count($membersmarketplace) != 0) {
         echo '<div class="post-box-holder flex-row"> <div class="row">';
         foreach ($membersmarketplace as $post) {
-            echo member_marketplace_grid($post->ID, hide_button: false, 'View Offer');
+            echo member_marketplace_grid($post->ID, false, 'View Offer');
         }
         echo '</div></div>';
     }
