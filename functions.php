59a817b8146b8e5c0fd35ba83fbd74d371e4341b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-30 12:02:53 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -161,7 +161,7 @@ function membership_listing($id = false, $allow_edit = false)
             <input type="hidden" name="edit" value="<?= $id ?>">
             <input type="hidden" name="submitted" value="true">
             <div class="form-group">
-                <label class="form-control"><span>Company: </span><input type="text" name="title" id="title" value=" <?= $title ?>"></label>
+                <label class="form-control"><span>Organisation: </span><input type="text" name="title" id="title" value=" <?= $title ?>"></label>
                 <label class="form-control"><span>Description: </span><textarea name="content" id="content"><?= $content_clean ?></textarea></label>
                 <label class="form-control"><span>Phone: </span><input type="tel" name="wpsl_phone" id="wpsl_phone" value=" <?= $wpsl_phone ?>"></label>
                 <label class="form-control"><span>Email: </span><input type="email" name="wpsl_email" id="wpsl_email" value=" <?= $wpsl_email ?>"></label>
