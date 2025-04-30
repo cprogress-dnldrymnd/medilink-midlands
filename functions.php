b9bd105ed1e7dafefdeef8f7b94a9e9889e19a26
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-30 11:19:34 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -136,87 +136,100 @@ function membership_listing($id = false, $allow_edit = false)
         $is_patron = false;
     }
 ?>
-    <div class="col-lg-12 post-item">
-        <div class="listing">
-            <div class="top">
-                <div class="row row-flex logo-heading">
-                    <?php if ($is_patron) { ?>
-                        <div class="col-lg-2 logo-box">
-                            <?= wp_get_attachment_image(50188, 'medium') ?>
-                        </div>
-                    <?php } ?>
-                    <div class="<?= $class ?> heading-description">
-                        <div class="heading-box">
-                            <h3>
-                                <?= get_the_title($post_id) ?>
-                                <?php if ($allow_edit == true) { ?>
-                                    <input type="title" value=" <?= get_the_title($post_id) ?>">
-                                <?php } ?>
-                            </h3>
-                        </div>
-                        <div class="description-box">
-                            <?= get_the_content(NULL, false, $post_id) ?>
-                        </div>
-                        <div class="meta-details">
-                            <ul>
-                                <?php if ($wpsl_phone) { ?>
-                                    <li>
-                                        <a href="tel:<?= $wpsl_phone ?>">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                                class="bi bi-telephone-fill" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd"
-                                                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-                                            </svg>
-                                            <?= $wpsl_phone ?>
-                                        </a>
-                                    </li>
-                                <?php } ?>
-                                <?php if ($wpsl_email) { ?>
-                                    <li>
-                                        <a href="mailto:<?= $wpsl_email ?>">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                                class="bi bi-envelope-fill" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
-                                            </svg>
-                                            <?= $wpsl_email ?>
-                                        </a>
-                                    </li>
-                                <?php } ?>
 
-                                <?php if ($wpsl_url) { ?>
-                                    <li>
-                                        <a href="<?= $url ?>" target="_blank">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                                class="bi bi-globe" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
-                                            </svg>
-                                            <?= getDomain($url) ?>
-                                        </a>
-                                    </li>
-                                <?php } ?>
+    <?php if ($allow_edit == true) { ?>
+        <form action="">
+        <?php } ?>
+        <div class="col-lg-12 post-item">
+            <div class="listing">
+                <div class="top">
+                    <div class="row row-flex logo-heading">
+                        <?php if ($is_patron) { ?>
+                            <div class="col-lg-2 logo-box">
+                                <?= wp_get_attachment_image(50188, 'medium') ?>
+                            </div>
+                        <?php } ?>
+                        <div class="<?= $class ?> heading-description">
+                            <div class="heading-box">
+                                <h3>
+                                    <?= get_the_title($post_id) ?>
+                                    <?php if ($allow_edit == true) { ?>
+                                        <input type="title" value=" <?= get_the_title($post_id) ?>">
+                                    <?php } ?>
+                                </h3>
+                            </div>
+                            <div class="description-box">
+                                <?= get_the_content(NULL, false, $post_id) ?>
+                            </div>
+                            <div class="meta-details">
+                                <ul>
+                                    <?php if ($wpsl_phone) { ?>
+                                        <li>
+                                            <a href="tel:<?= $wpsl_phone ?>">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                    class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd"
+                                                        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                                                </svg>
+                                                <?= $wpsl_phone ?>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
+                                    <?php if ($wpsl_email) { ?>
+                                        <li>
+                                            <a href="mailto:<?= $wpsl_email ?>">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                    class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+                                                </svg>
+                                                <?= $wpsl_email ?>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
 
-                            </ul>
-                        </div>
-                        <div class="modeltheme_button view-all">
-                            <a href="<?= $url ?>" target="_blank" class="button-winona button-green btn btn-sm">
-                                Visit Website
-                            </a>
+                                    <?php if ($wpsl_url) { ?>
+                                        <li>
+                                            <a href="<?= $url ?>" target="_blank">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                    class="bi bi-globe" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
+                                                </svg>
+                                                <?= getDomain($url) ?>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
 
-                            <?php if ($allow_edit == true) { ?>
-                                <a href="?profiletab=directory&edit=<?= $id ?>" class="button-winona button-accent btn btn-sm">
-                                    Edit
+                                </ul>
+                            </div>
+                            <div class="modeltheme_button view-all">
+                                <a href="<?= $url ?>" target="_blank" class="button-winona button-green btn btn-sm">
+                                    Visit Website
                                 </a>
-                            <?php } ?>
+
+                                <?php if ($allow_edit == true) { ?>
+                                    <?php if (isset($_GET['edit']) && $_GET['edit'] == $id) { ?>
+                                        <button type="submit" class="button-winona button-accent btn btn-sm">
+                                            Submit
+                                        </button>
+                                    <?php } else { ?>
+                                        <a href="?profiletab=directory&edit=<?= $id ?>" class="button-winona button-accent btn btn-sm">
+                                            Edit
+                                        </a>
+                                    <?php } ?>
+                                <?php } ?>
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
-
         </div>
-    </div>
+        <?php if ($allow_edit == true) { ?>
+        </form>
+    <?php } ?>
 <?php
     return ob_get_clean();
 }
