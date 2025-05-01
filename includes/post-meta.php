9f1e26d5d576e35695e185b6b662fd5106a02b7d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-01 23:36:23 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -128,7 +128,11 @@ Container::make('theme_options', __('Settings'))
 
 Container::make('theme_options', __('Email Settings'))
 	->set_page_parent('edit.php?post_type=wpsl_stores')
-	->add_tab('Directory Submitted', array(
+	->add_tab('Directory Submitted Admin Email', array(
 		Field::make('text', 'member_directory_submitted_admin_email_subject', __('Admin Email Subject')),
 		Field::make('rich_text', 'member_directory_submitted_admin_email_message', __('Admin Email Message')),
+	))
+	->add_tab('Directory Submitted Client Email', array(
+		Field::make('text', 'member_directory_submitted_client_email_subject', __('Client Email Subject')),
+		Field::make('rich_text', 'member_directory_submitted_client_email_message', __('Client Email Message')),
 	));
