a77c2e3484b487f4d5e5adcf9c85a6473173f8ad
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-01 19:12:54 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1598,9 +1598,7 @@ function user_directory()
         'post_status' => array('publish', 'private', 'pending')
     ));
 
-    echo get__current_user_email();
-    echo 'xx';
-    echo get__current_user_username();
+   
     echo '<div class="user-posts marketplace-posts">';
     echo '<h3 class="main-heading">Membership Directory</h3>';
     echo '<div class="membership-directory">';
