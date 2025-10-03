af6d9c5a000e0b539b4cf54bc1d0c050a0946943
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-03 18:00:38 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -337,6 +337,55 @@ add_filter('register_post_type_args', 'modify_wpsl_stores_admin_label', 10, 2);
 
 
 
+$Events = new newPostType();
+$Events->key = 'events';
+$Events->name = 'Events';
+$Events->singular_name = 'Event';
+$Events->icon = 'dashicons-calendar';
+$Events->supports = array('title', 'editor', 'revisions', 'thumbnail', 'author');
+$Events->exclude_from_search = false;
+$Events->publicly_queryable = true;
+$Events->show_in_admin_bar = true;
+$Events->has_archive = true;
+$Events->rewrite = array(
+    'with_front' => false,
+    'slug'       => 'event',
+);
+
+$Events_Categories = new newTaxonomy();
+$Events_Categories->taxonomy = 'events_category';
+$Events_Categories->post_type = 'events';
+$Events_Categories->args = array(
+    'label'        => 'Categories',
+    'labels'       => array(
+        'name'                       => _x('Categories', 'Taxonomy General Name', 'text_domain'),
+        'singular_name'              => _x('Category', 'Taxonomy Singular Name', 'text_domain'),
+        'menu_name'                  => __('Category', 'text_domain'),
+        'all_items'                  => __('All Items', 'text_domain'),
+        'parent_item'                => __('Parent Item', 'text_domain'),
+        'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+        'new_item_name'              => __('New Item Name', 'text_domain'),
+        'add_new_item'               => __('Add New Item', 'text_domain'),
+        'edit_item'                  => __('Edit Item', 'text_domain'),
+        'update_item'                => __('Update Item', 'text_domain'),
+        'view_item'                  => __('View Item', 'text_domain'),
+        'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
+        'add_or_remove_items'        => __('Add or remove items', 'text_domain'),
+        'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+        'popular_items'              => __('Popular Items', 'text_domain'),
+        'search_items'               => __('Search Items', 'text_domain'),
+        'not_found'                  => __('Not Found', 'text_domain'),
+        'no_terms'                   => __('No items', 'text_domain'),
+        'items_list'                 => __('Items list', 'text_domain'),
+        'items_list_navigation'      => __('Items list navigation', 'text_domain'),
+    ),
+    'show_in_rest' => true,
+    'hierarchical' => true,
+    'query_var'    => false,
+    'has_archive'  => false,
+    'rewrite'      => false,
+);
+
 
 $Opportunities = new newPostType();
 $Opportunities->key = 'opportunities';
